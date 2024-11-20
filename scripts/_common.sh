0aa831f837a8128a121c2c3f3008641ebe1ba118
Fatih Bıyıklı <fatih@example.com>
2024-11-21 00:47:16 +0300

    Add _common.sh for installation dependencies

---
+++ scripts/_common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="nodejs"
